c48f9e386cf4d8a5c0695e7f7ae0d248888c9caa
piyushjakhar62-ctrl <piyushjakhar62@gmail.com>
2026-05-25 17:49:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -215,6 +215,6 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
+server.listen(process.env.PORT || 3000, () => {
   console.log('ZapChat chal raha hai! Port 3000');
 });
